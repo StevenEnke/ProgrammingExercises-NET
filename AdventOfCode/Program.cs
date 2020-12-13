c559559f0600b268dd28a21711f1326016f60f3b
Steven Enke <stevenke73@gmail.com>
2020-12-12 23:46:44 -0600

    Creating AdventForCode selection menu.

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        private static IEnumerable<string> exitOptions = new List<string>
+        {
+            "exit",
+            "quit",
+            ":q"
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"You may exit at anytime using {string.Join(", ", exitOptions)}");
+
+            string userInput = string.Empty;
+            do
+            {
+                Console.Write("Choose a year: ");
+                userInput = Console.ReadLine();
+
+                if (String.Equals(userInput, "2020", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    Year2020(ref userInput);
+                }
+
+            } while (!IsExitCondition(userInput));
+        }
+
+        private static void Year2020(ref string userInput)
+        {
+            do
+            {
+                Console.WriteLine("    1) Report Repair");
+                Console.WriteLine("Choose a Day: ");
+                userInput = Console.ReadLine();
+
+                if (userInput.TrimStart().StartsWith('1'))
+                {
+
+                }
+            } while (!IsExitCondition(userInput) && !IsBreakCondition(userInput));
+        }
+
+        private static bool IsBreakCondition(string userInput)
+        {
+            return String.Equals(userInput, "break", StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        private static bool IsExitCondition(string userInput)
+        {
+            return exitOptions.Any(exitOption => String.Equals(userInput, exitOption, StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ProgrammingExercises-NET
+
+Implementations of various programming excercises - such as those from AdventOfCode or /r/learnprogramming/ - implemented using C#/.NET
