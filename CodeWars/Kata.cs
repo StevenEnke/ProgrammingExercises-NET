c2e485bde178e25a419becfdfd99f5db7c5e39e1
Steven Enke <stevenke73@gmail.com>
2020-12-31 21:12:03 -0600

    Fixing a CodeWars problem to figure out how to generate a string of unique characters from 2 input strings.

---
+++ CodeWars/Kata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string Solve(string a, string b)
+    {
+        var aCharacterSet = a.Select(c => c);
+        var bCharacterSet = b.Select(c => c);
+        var commonElements = a.Intersect(b);
+        var uniqueElements = (a.Concat(b)).Where(c => !commonElements.Contains(c));
+        return string.Join(string.Empty, uniqueElements);
+    }
+}
